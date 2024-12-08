a8c8887d6caf6624b828c38f0e04f2681f828088
Hans Krutzer <hans@seamly.ai>
2024-12-08 16:05:24 +0100

    Serialize datetimes as ISO8601 strings

--- lib/term_to_json/decoder.ex
+++ lib/term_to_json/decoder.ex
@@ -4,8 +4,28 @@ defmodule TermToJson.Decoder do
     :json.decode(json, :ok, %{
       null: nil,
       string: fn
-        ":" <> x -> String.to_atom(x)
-        x -> x
+        ":" <> x ->
+          String.to_atom(x)
+
+        # ISO 8601 datetime pattern
+        <<_y::binary-size(4), "-", _m::binary-size(2), "-", _d::binary-size(2), "T",
+          _h::binary-size(2), ":", _min::binary-size(2), ":", _s::binary-size(2),
+          rest::binary>> = dt
+        when rest == "Z" or binary_part(rest, byte_size(rest) - 1, 1) == "Z" ->
+          case DateTime.from_iso8601(dt) do
+            {:ok, datetime, _offset} -> datetime
+            _ -> dt
+          end
+
+        # ISO 8601 date pattern
+        <<_y::binary-size(4), "-", _m::binary-size(2), "-", _d::binary-size(2)>> = date ->
+          case Date.from_iso8601(date) do
+            {:ok, date} -> date
+            _ -> date
+          end
+
+        x ->
+          x
       end,
       array_finish: fn acc, old_acc ->
         acc = :lists.reverse(acc)
